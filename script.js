b1bf3a09f9efbadd4b538e08f37d5d9ac7ef09c4
Chang Zhao <chang-zhao@users.noreply.github.com>
2018-11-13 14:32:15 +0300

    Bugs changed, debug logging removed

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 window.onload=function() {
     document.body.addEventListener('click', function (event) {
-        var a = document.querySelector(':hover');
-        if (!a) return;
+        var a = event.target;
         if (a.classList.contains("wikilink1") || a.classList.contains("wikilink2")) {
             var xy = a.getBoundingClientRect(),
-                dx = xy.right - 12 - event.offsetX,
+                dx = xy.width - event.offsetX,
                 dy = event.offsetY;
-            if (dx*dx + dy*dy < 169) { a.href += '?do=edit'; }
-            console.log(xy);
-            console.log(event.offsetX+" * "+event.offsetY+"; dx= "+dx+", dy="+dy);
-            console.log(event.target);
+            if (dx*dx + dy*dy < 169) a.href += '?do=edit';
         }
-        console.log(a);
     });
-};
+}
