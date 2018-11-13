9c8023279e7fbf42a7c53781fc2ac0b672681dac
Chang Zhao <chang-zhao@users.noreply.github.com>
2018-11-13 20:39:55 +0300

    Added breadcrumbs class

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload=function() {
     document.body.addEventListener('click', function (event) {
         var a = event.target;
-        if (a.classList.contains("wikilink1") || a.classList.contains("wikilink2")) {
+        if (a.classList.contains("wikilink1") || a.classList.contains("wikilink2") || a.classList.contains("breadcrumbs")) {
             var xy = a.getBoundingClientRect(),
                 dx = xy.width - event.offsetX,
                 dy = event.offsetY;
