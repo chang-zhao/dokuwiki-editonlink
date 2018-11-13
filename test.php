f068fc6f67af7ff22cd4bbabf08a454535b20cde
Chang Zhao <admin@zen-do.ru>
2018-11-13 11:42:56 +0300

    ready clean

--- test.php
+++ test.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * editonlink
+ * EditOnLink plugin
+ * Test file
+ * @author Constant Illumination (Chang Zhao) <alex@obschy.ru>
  * Date: 13.11.2018
  */
 ?>
@@ -10,8 +12,8 @@
     <script src="script.js"></script>
 </head>
 <body>
-asasa
+<p>Hover this link; see an animated decoration and a popover button.</p>
 <div><a href="test.php" class="wikilink1">ASSA MASSA</a></div>
-masasa
+<p>Clicking that button must turn URL "test.php" into "test.php?do=edit".</p>
 </body>
 </html>
