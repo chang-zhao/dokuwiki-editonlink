7e3af9d23733875004ed07811523e08af185af50
Chang Zhao <chang-zhao@users.noreply.github.com>
2018-11-13 21:22:57 +0300

    Update test.php

--- test.php
+++ test.php
@@ -12,7 +12,7 @@
     <script src="script.js"></script>
 </head>
 <body style="padding-left:6em;<?php if($_GET['do']=='edit') echo 'color:#369;'; ?>">
-<?php echo '$_GET = '.var_export($_GET, true); ?>
+<?php echo '$_GET = '.htmlspecialchars(var_export($_GET, true), ENT_QUOTES); ?>
 <p>Hover this link; see an animated<br>
 decoration and a popover button.</p>
 <div><a href="test.php" class="wikilink1">LINK TO TEST.PHP</a></div>
