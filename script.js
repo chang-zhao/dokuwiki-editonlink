6c2999fe9e7f352c7daced3ef93f61809ac59b26
Chang Zhao <chang-zhao@users.noreply.github.com>
2018-11-14 00:45:25 +0300

    Set button size depending on font size (#2)
    * Set button size depending on font size
    
    * Update style.css

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 window.onload=function() {
     document.body.addEventListener('click', function (event) {
-        var a = event.target;
-        if (a.classList.contains("wikilink1") || a.classList.contains("wikilink2") || a.classList.contains("breadcrumbs")) {
+        var a = event.target || event.srcElement;
+        if (a.tagName === 'A' && (a.classList.contains("wikilink1") || a.classList.contains("wikilink2") || a.classList.contains("breadcrumbs"))) {
             var xy = a.getBoundingClientRect(),
                 dx = xy.width - event.offsetX,
-                dy = event.offsetY;
-            if (dx*dx + dy*dy < 169) a.href += '?do=edit';
+                dy = event.offsetY,
+                r = parseFloat(window.getComputedStyle(a, ':after')['height'])/2 + 1;
+            if (dx*dx + dy*dy < r*r) a.href += '?do=edit';
         }
     });
 };
