1746366248132949fa88a1c653b39d33e4ef405e
Chang Zhao <chang-zhao@users.noreply.github.com>
2018-11-17 18:52:50 +0300

    v1.1. Support for multiline links, ctrl-click, right-click (#3)
    In case of multiline link, clientBoundingRect top-right might not give the correct position of the button; so I use adding another element with the same positioning on the link click, to learn the actual position.
    
    As for ctrl-click and right-click, the user can stay on the page, so i restore the target link to its original href (onmouseleave).
    
    * Added test links with request parameters and anchors
    
    * Added an element to find the button position
    
    span#plugin__editonlink having the same positioning as ":after" can be used to find its position even in case of multiline target link
    
    * Support for multiline links, ctrl-click, right-click
    
    * Update plugin.info.txt

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 window.onload=function() {
-    document.body.addEventListener('click', function (event) {
-        var a = event.target || event.srcElement;
-        if (a.tagName === 'A' && (a.classList.contains("wikilink1") || a.classList.contains("wikilink2") || a.classList.contains("breadcrumbs"))) {
-            var xy = a.getBoundingClientRect(),
-                dx = xy.width - event.offsetX,
-                dy = event.offsetY,
-                r = parseFloat(window.getComputedStyle(a, ':after')['height'])/2 + 1;
-            if (dx*dx + dy*dy < r*r) a.href = (a.href.split('#', 1))[0] + '?do=edit';
-        }
-    });
+    document.body.addEventListener('click', editOnLink);
+    document.body.addEventListener('contextmenu', editOnLink);
 };
+function editOnLink(e) {
+    var a = e.target || e.srcElement;
+    if (a.tagName === 'A' && (a.classList.contains("wikilink1") || a.classList.contains("wikilink2") || a.classList.contains("breadcrumbs"))) {
+
+        // In case of multy-line link, simple getBoundingClientRect wouldn't work well,
+        // so we find the spot by adding another element with the same positioning as
+        // that ":after" button. Thus we know where the popover button is.
+        var v = document.createElement('span');
+        v.id = 'plugin__editonlink';
+        a.appendChild(v);
+        var xy = v.getBoundingClientRect(),
+            x = xy.right,
+            y = xy.top,                         // x,y = of the popover button center
+            xya = a.getBoundingClientRect(),    // the mouse click was related to <a>
+            dx = e.offsetX + xya.left - x,      // how far the click was from the button?
+            dy = e.offsetY + xya.top - y,
+            r = parseFloat(window.getComputedStyle(a, ':after')['height'])/2 + 1;
+        if ((dx*dx + dy*dy) < r*r) {
+            a.setAttribute('data-editonlink', a.href);          // maybe ctrl-click etc
+            a.href = (a.href.split(/[?#]/))[0] + '?do=edit';    // add EDIT to the link
+            v.remove();
+            a.addEventListener('mouseleave', editOnLinkOut);    // to restore the link back
+        }
+    }
+}
+function editOnLinkOut(e) {
+    var a = e.target || e.srcElement;
+    if (a.tagName !== 'A') return;
+    var d = a.getAttribute('data-editonlink');
+    if (d) {
+        a.href = d;
+        a.removeAttribute('data-editonlink');
+    }
+    a.removeEventListener('mouseleave');
+}
