4c225183a340f00bcaa2e29b6e56b879b130fcf0
Chang Zhao <chang-zhao@users.noreply.github.com>
2018-12-12 07:57:14 +0300

    For compliance

--- test.php
+++ test.php
@@ -8,13 +8,14 @@
 ?>
 <html lang="en">
 <head>
+    <title>EditOnLink Test</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="script.js"></script>
 </head>
 <body style="padding-left:6em;<?php if($_GET['do']=='edit')
     echo 'color:#369; background:#FFFFCC;'; ?>" class="dokuwiki">
 <?php echo '$_GET = '.htmlspecialchars(var_export($_GET, true), ENT_QUOTES); ?>
-<p>Hover this link; see an animated<br>
+<p id="top">Hover this link; see an animated<br>
 decoration and a popover button.</p>
 <div><a href="test.php" class="wikilink1">LINK TO TEST.PHP</a></div>
 <p>Clicking that button must turn URL<br>
