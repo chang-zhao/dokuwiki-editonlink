0aae8f7db72d9df4644414eccebc381e991116ea
Chang Zhao <chang-zhao@users.noreply.github.com>
2019-02-15 16:25:40 +0300

    href parameters not removed

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ function editOnLink(e) {
             xya = a.getBoundingClientRect(),    // the mouse click was related to <a>
             dx = e.offsetX + xya.left - x,      // how far the click was from the button?
             dy = e.offsetY + xya.top - y,
+            aUrl = (a.href.split(/#/))[0],      // link href (without anchor hash)
             r = parseFloat(window.getComputedStyle(a, ':after')['height'])/2 + 1;
         if ((dx*dx + dy*dy) < r*r) {
             a.setAttribute('data-editonlink', a.href);          // maybe ctrl-click etc
-            a.href = (a.href.split(/[?#]/))[0] + '?do=edit';    // add EDIT to the link
+            if (aUrl.indexOf('?') < 0) a.href += '?do=edit';   // add EDIT to the link
+            else a.href += '&do=edit';
             v.remove();
             a.addEventListener('mouseleave', editOnLinkOut);    // to restore the link back
         }
